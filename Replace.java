2cd1b51a1afc576b774477123bc7b2754c0970a6
Snehakaku26 <126590374+Snehakaku26@users.noreply.github.com>
2023-03-03 08:29:25 +0530

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Replace{
+    public  static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the String:");
+        String s=sc.nextLine();
+     String[] words;
+      words=s.split("");
+     for( String a:words)
+   
+     System.out.println(a);
+   
+
+    }
+}
+
+    
+    
